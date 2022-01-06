fa251fd285b3bbd050d6e10b49fe89b0af2913e5
mhicauber <mhicauber@okina.fr>
2022-01-06 13:54:26 +0100

    added flag for skipping tests (default : false)

--- build.sh
+++ build.sh
@@ -1,14 +1,20 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 
 echo Building docker image
+
+# Arguments
 
 PARAMS=""
 while (( "$#" )); do
   case "$1" in
     -s|--skip-tests)
-      SKIP_TESTS=true
-      shift
+      if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+        SKIP_TESTS=$2
+        shift 2
+      else
+        echo "Error: Argument for $1 is missing" >&2
+        exit 1
+      fi
       ;;
     -*|--*=) # unsupported flags
       echo "Error: Unsupported flag $1" >&2
@@ -25,14 +31,14 @@ eval set -- "$PARAMS"
 
 echo Skipping tests : ${SKIP_TESTS:=false}
 
-# Back
-VERSION_BACK=$(mvn -q \
-    -Dexec.executable=echo \
-    -Dexec.args='${project.version}' \
-    --non-recursive \
-    exec:exec)
-BACK_IMAGE_NAME=registry.okina.fr/mobiiti/anshar:"${VERSION_BACK}"
-#mvn spring-boot:build-image -Dspring-boot.build-image.imageName="${BACK_IMAGE_NAME}" -D${SKIP_TESTS}
-mvn clean package -D${SKIP_TESTS}
-docker build -t "${BACK_IMAGE_NAME}" --build-arg JAR_FILE=target/anshar-${VERSION_BACK}.jar .
-docker push "${BACK_IMAGE_NAME}"
+## Back
+#VERSION_BACK=$(mvn -q \
+#    -Dexec.executable=echo \
+#    -Dexec.args='${project.version}' \
+#    --non-recursive \
+#    exec:exec)
+#BACK_IMAGE_NAME=registry.okina.fr/mobiiti/anshar:"${VERSION_BACK}"
+##mvn spring-boot:build-image -Dspring-boot.build-image.imageName="${BACK_IMAGE_NAME}" -D${SKIP_TESTS}
+#mvn clean package -D${SKIP_TESTS}
+#docker build -t "${BACK_IMAGE_NAME}" --build-arg JAR_FILE=target/anshar-${VERSION_BACK}.jar .
+#docker push "${BACK_IMAGE_NAME}"
