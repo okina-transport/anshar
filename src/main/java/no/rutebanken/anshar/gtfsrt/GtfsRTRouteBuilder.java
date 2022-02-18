4574af44438d0c9efb88887bd563048d9f24decd
gcarrere <gcarrere@okina.fr>
2022-02-18 16:56:35 +0100

    add logs on GTFS-RT

--- src/main/java/no/rutebanken/anshar/gtfsrt/GtfsRTRouteBuilder.java
+++ src/main/java/no/rutebanken/anshar/gtfsrt/GtfsRTRouteBuilder.java
@@ -4,11 +4,15 @@ import no.rutebanken.anshar.config.AnsharConfiguration;
 import no.rutebanken.anshar.routes.BaseRouteBuilder;
 import no.rutebanken.anshar.subscription.SubscriptionManager;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GtfsRTRouteBuilder extends BaseRouteBuilder{
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private static final int INTERVAL_IN_MILLIS = 60000;
 
@@ -27,6 +31,8 @@ public class GtfsRTRouteBuilder extends BaseRouteBuilder{
                     "import_GTFSRT_DATA")
                     .bean(GtfsRTDataRetriever.class,"getGTFSRTData")
                     .end();
+        }else{
+            logger.info("Pas d'url GTFS-RT définie");
         }
 
     }
