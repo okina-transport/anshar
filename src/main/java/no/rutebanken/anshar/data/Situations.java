f6ba44ee4033e9315dd851bf1be567be2d239972
gcarrere <gcarrere@okina.fr>
2023-10-25 15:06:22 +0200

    Fix merge

--- src/main/java/no/rutebanken/anshar/data/Situations.java
+++ src/main/java/no/rutebanken/anshar/data/Situations.java
@@ -31,6 +31,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
+import uk.org.ifopt.siri20.StopPlaceRef;
 import uk.org.siri.siri20.*;
 
 import javax.annotation.PostConstruct;
@@ -69,6 +70,9 @@ public class Situations extends SiriRepository<PtSituationElement> {
 
     @Autowired
     ExtendedHazelcastService hazelcastService;
+
+    @Autowired
+    private StopPlaceUpdaterService stopPlaceService;
 
     protected Situations() {
         super(SiriDataType.SITUATION_EXCHANGE);
@@ -348,6 +352,7 @@ public class Situations extends SiriRepository<PtSituationElement> {
                 updated = true;
             }
             timingTracer.mark("compareChecksum");
+            updated = defineAffectedPoints(situation, datasetId) || updated;
 
             if (keepByProgressStatus(situation) && updated) {
                 timingTracer.mark("keepByProgressStatus");
@@ -396,6 +401,38 @@ public class Situations extends SiriRepository<PtSituationElement> {
 
         return changes.values();
     }
+
+    private boolean defineAffectedPoints(PtSituationElement situation, String datasetId) {
+
+        if (situation.getAffects() == null || situation.getAffects().getStopPoints() == null
+                || situation.getAffects().getStopPoints().getAffectedStopPoints() == null) {
+            return false;
+        }
+        List<AffectedStopPointStructure> refId = situation.getAffects().getStopPoints().getAffectedStopPoints();
+        List<AffectedStopPointStructure> refIdStopPlace = refId.stream()
+                .filter(affectedStopPoint -> affectedStopPoint.getStopPointRef() != null && stopPlaceService.isKnownId(datasetId + ":StopPlace:" + affectedStopPoint.getStopPointRef().getValue()))
+                .collect(Collectors.toList());
+        List<AffectedStopPlaceStructure> affectedStopPlaceStructures = new ArrayList<>();
+
+        refIdStopPlace.forEach(affectedStopPoint -> {
+            AffectedStopPlaceStructure affectedStopPlaceStructure = new AffectedStopPlaceStructure();
+            StopPlaceRef stopPlaceRef = new StopPlaceRef();
+            stopPlaceRef.setValue(affectedStopPoint.getStopPointRef().getValue());
+            affectedStopPlaceStructure.setStopPlaceRef(stopPlaceRef);
+            affectedStopPlaceStructures.add(affectedStopPlaceStructure);
+        });
+
+        situation.getAffects().getStopPoints().getAffectedStopPoints().removeAll(refIdStopPlace);
+        if (!affectedStopPlaceStructures.isEmpty()) {
+
+            AffectsScopeStructure.StopPlaces newStopPlaces = new AffectsScopeStructure.StopPlaces();
+            newStopPlaces.getAffectedStopPlaces().addAll(affectedStopPlaceStructures);
+            situation.getAffects().setStopPlaces(newStopPlaces);
+            return true;
+        }
+        return false;
+    }
+
 
     public void removeSituation(String datasetId, PtSituationElement situation) {
 
