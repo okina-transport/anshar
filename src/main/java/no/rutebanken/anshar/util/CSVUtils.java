82227ccc4b7d17f2cd1f01a270306d4d9a0d8b0e
gcarrere <gcarrere@okina.fr>
2024-01-12 16:33:00 +0100

    Fix delimiter for CSV

--- src/main/java/no/rutebanken/anshar/util/CSVUtils.java
+++ src/main/java/no/rutebanken/anshar/util/CSVUtils.java
@@ -2,23 +2,17 @@ package no.rutebanken.anshar.util;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.io.IOUtils;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class CSVUtils {
     /**
      * Read a csv file and builds a collection of records
-     * @param file
-     *  the file to read
-     * @return
-     *  a collection of records
+     *
+     * @param file the file to read
+     * @return a collection of records
      * @throws IOException
      */
     public static Iterable<CSVRecord> getRecords(File file) throws IOException {
@@ -32,7 +26,11 @@ public class CSVUtils {
         }
         baos.flush();
 
+        InputStream is1 = new ByteArrayInputStream(baos.toByteArray());
         InputStream is2 = new ByteArrayInputStream(baos.toByteArray());
+        String result = IOUtils.toString(is1, StandardCharsets.UTF_8);
+
+        String delimiter = guessDelimiter(result);
 
         Reader reader = new InputStreamReader(is2);
 
@@ -40,8 +38,25 @@ public class CSVUtils {
                 .builder()
                 .setHeader()
                 .setSkipHeaderRecord(false)
-                .setDelimiter(";")
+                .setDelimiter(delimiter)
                 .build()
                 .parse(reader);
+    }
+
+    private static String guessDelimiter(String fileContent) {
+
+        String[] lines = fileContent.split("\n");
+        String firstLine = lines[0];
+        long nbOfSemiColon = firstLine.chars()
+                .filter(ch -> ch == ';')
+                .count();
+
+        long nbOfComma = firstLine.chars()
+                .filter(ch -> ch == ',')
+                .count();
+
+        return nbOfSemiColon > nbOfComma ? ";" : ",";
+
+
     }
 }
