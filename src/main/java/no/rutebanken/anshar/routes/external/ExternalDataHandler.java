c06ed1413507d9f951755f63791ba62babe801c6
mhicauber <mhicauber@okina.fr>
2024-11-22 09:24:57 +0100

    fix log message

--- src/main/java/no/rutebanken/anshar/routes/external/ExternalDataHandler.java
+++ src/main/java/no/rutebanken/anshar/routes/external/ExternalDataHandler.java
@@ -111,7 +111,7 @@ public class ExternalDataHandler {
             String url = e.getIn().getHeader(URL_HEADER_NAME, String.class);
 
             if (StringUtils.isEmpty(datasetId)) {
-                logger.error("No datasetId were specified in external SM data");
+                logger.error("No datasetId were specified in external ET data");
                 return;
             }
 
