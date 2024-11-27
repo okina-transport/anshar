2655ab01c0842842118a100157b044dc09b4edbe
jmarzin1 <julien.marzin@lumiplan.com>
2024-11-27 10:59:22 +0100

    feat(DATA-650): upgrade camel version and avoid quartz initialization error

--- src/main/java/no/rutebanken/anshar/config/AnsharConfiguration.java
+++ src/main/java/no/rutebanken/anshar/config/AnsharConfiguration.java
@@ -18,16 +18,11 @@ package no.rutebanken.anshar.config;
 import com.hazelcast.map.IMap;
 import lombok.Getter;
 import lombok.Setter;
-import org.apache.camel.CamelContext;
-import org.apache.camel.component.micrometer.messagehistory.MicrometerMessageHistoryFactory;
-import org.apache.camel.component.micrometer.routepolicy.MicrometerRoutePolicyFactory;
-import org.apache.camel.spring.boot.CamelContextConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
@@ -182,20 +177,4 @@ public class AnsharConfiguration {
         TimeZone.setDefault(TimeZone.getTimeZone(defaultTimeZone));
     }
 
-    @Bean
-    public CamelContextConfiguration camelContextConfiguration() {
-
-        return new CamelContextConfiguration() {
-            @Override
-            public void beforeApplicationStart(CamelContext camelContext) {
-                camelContext.addRoutePolicyFactory(new MicrometerRoutePolicyFactory());
-                camelContext.setMessageHistoryFactory(new MicrometerMessageHistoryFactory());
-            }
-
-            @Override
-            public void afterApplicationStart(CamelContext camelContext) {
-
-            }
-        };
-    }
 }
