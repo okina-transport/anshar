6e30cb06ce46c1dda4c46bcb704de2ca08f613cc
afreche <alexandre.freche@lumiplan.com>
2025-03-12 10:43:16 +0100

    refactor: TokenService

--- src/main/java/no/rutebanken/anshar/config/TokenService.java
+++ src/main/java/no/rutebanken/anshar/config/TokenService.java
@@ -7,40 +7,35 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import static org.keycloak.OAuth2Constants.CLIENT_CREDENTIALS;
+
 @Service
-public final class TokenService {
-    private static Keycloak keycloakClient;
+public class TokenService {
     private static final Logger log = LoggerFactory.getLogger(TokenService.class);
 
-    @Value("${iam.keycloak.admin.client}")
-    private String clientId;
+    private final Keycloak keycloakClient;
 
-    @Value("${iam.keycloak.client.secret}")
-    private String clientSecret;
-
-    @Value("${keycloak.realm}")
-    private String realm;
-
-    @Value("${keycloak.auth-server-url}")
-    private String authServerUrl;
-
-    private TokenService() {
-        log.info("Token service initialized");
-        log.info("clientId:" + clientId);
-        log.info("realm:" + realm);
-        log.info("authServerUrl:" + authServerUrl);
-
+    public TokenService(@Value("${iam.keycloak.admin.client}") String clientId,
+                        @Value("${iam.keycloak.client.secret}") String clientSecret,
+                        @Value("${keycloak.realm}") String realm,
+                        @Value("${keycloak.auth-server-url}") String authServerUrl) {
+        log.debug("Build keycloak client with url {} and realm {}", authServerUrl, realm);
+        this.keycloakClient = KeycloakBuilder.builder()
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .realm(realm)
+                .serverUrl(authServerUrl)
+                .grantType(CLIENT_CREDENTIALS)
+                .build();
     }
 
     public String getToken() {
-
-        if (keycloakClient == null ){
-
-            keycloakClient = KeycloakBuilder.builder().clientId(clientId).clientSecret(clientSecret).realm(realm).serverUrl(authServerUrl).grantType("client_credentials").build();
+        try {
+            return keycloakClient.tokenManager().getAccessTokenString();
+        } catch (Exception e){
+            log.error("Error while getting token", e);
+            return "emptyToken";
         }
-
-        return keycloakClient.tokenManager().getAccessTokenString();
     }
-
 
 }
