b09641aeb71a5f2b0f1a2dbc350d356a80fafc3f
poswald <poswald@okina.fr>
2025-04-16 17:12:23 +0200

    add X-Gravitee-Api-Key to rest configuration for header property

--- src/main/java/no/rutebanken/anshar/routes/RestRouteBuilder.java
+++ src/main/java/no/rutebanken/anshar/routes/RestRouteBuilder.java
@@ -95,7 +95,7 @@ public class RestRouteBuilder extends RouteBuilder {
                 .enableCORS(true)
                 .corsAllowCredentials(true)
                 .corsHeaderProperty("Access-Control-Allow-Origin", "*")
-                .corsHeaderProperty("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization")
+                .corsHeaderProperty("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, X-Gravitee-Api-Key")
         ;
 
         onException(ConnectException.class)
