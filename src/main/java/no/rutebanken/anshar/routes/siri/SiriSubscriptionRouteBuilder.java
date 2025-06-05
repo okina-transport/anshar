0575433ecb19be42443f736926cc405f8a125dfe
gcarrere <gcarrere@okina.fr>
2025-06-05 10:23:48 +0200

    Increase monitor interval

--- src/main/java/no/rutebanken/anshar/routes/siri/SiriSubscriptionRouteBuilder.java
+++ src/main/java/no/rutebanken/anshar/routes/siri/SiriSubscriptionRouteBuilder.java
@@ -101,7 +101,7 @@ public abstract class SiriSubscriptionRouteBuilder extends BaseRouteBuilder {
 //            hasBeenStarted = subscriptionSetup.isActive();
         }
 
-        singletonFrom("quartz://anshar/monitor_" + subscriptionSetup.getSubscriptionId() + "?trigger.repeatInterval=" + 15000,
+        singletonFrom("quartz://anshar/monitor_" + subscriptionSetup.getSubscriptionId() + "?trigger.repeatInterval=" + 60000,
                 "monitor.subscription." + subscriptionSetup.getVendor())
                 .choice()
                 .when(p -> shouldPerformDataNotReceivedAction(p.getFromRouteId()))
