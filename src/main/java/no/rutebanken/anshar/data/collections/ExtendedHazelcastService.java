9adb0eba6d87f66658221ad9a3bbc2fa1a91b2c1
gcarrere <gcarrere@okina.fr>
2026-03-10 12:09:50 +0100

    REmove DATA-1550

--- src/main/java/no/rutebanken/anshar/data/collections/ExtendedHazelcastService.java
+++ src/main/java/no/rutebanken/anshar/data/collections/ExtendedHazelcastService.java
@@ -184,14 +184,7 @@ public class ExtendedHazelcastService extends HazelCastService {
     }
 
 
-    public IMap<String, Pair<LocalDateTime, String>> getScheduledGeneralMessages(String dataset) {
-        return hazelcast.getMap("anshar.scheduled.general.messages." + dataset);
-    }
 
-    @Bean
-    public IScheduledExecutorService getSharedScheduler() {
-        return hazelcast.getScheduledExecutorService("ansharScheduler");
-    }
 
 
     public ISet<String> getSharedSMDatasetList() {
