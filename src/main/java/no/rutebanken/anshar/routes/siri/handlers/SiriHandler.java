48c753031d018797c600d9b1c164e9d886459cc7
afreche <alexandre.freche@lumiplan.com>
2026-04-21 09:52:55 +0200

    Revert "fix: DATA-1729 no longer carry the FlexibleLine by the line ID"
    This reverts commit b5141fd1be87e1e4c9f67026bdf7128440409d1e.

--- src/main/java/no/rutebanken/anshar/routes/siri/handlers/SiriHandler.java
+++ src/main/java/no/rutebanken/anshar/routes/siri/handlers/SiriHandler.java
@@ -198,6 +198,7 @@ public class SiriHandler {
 
     public Siri buildSiriResponse(IncomingSiriParameters incomingSiriParameters, Siri request) {
         Siri response = outboundProcessSiriServerRequest(request, incomingSiriParameters);
+        utils.handleFlexibleLines(response);
         incomingSiriParameters.setVersion(request.getVersion());
         return response;
     }
