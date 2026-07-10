39f4e459e0acfa0da73dd96749dbaa25ec7c40d1
jmarzin1 <julien.marzin@lumiplan.com>
2026-07-10 17:16:35 +0200

    fix(DATA-2041) : set subscriptionRef from subscription instead of default value in outbound siri

--- src/main/java/no/rutebanken/anshar/routes/outbound/OutboundSubscriptionSetup.java
+++ src/main/java/no/rutebanken/anshar/routes/outbound/OutboundSubscriptionSetup.java
@@ -19,6 +19,7 @@ import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import lombok.Getter;
 import lombok.Setter;
+import no.rutebanken.anshar.routes.outbound.model.CompressionFormat;
 import no.rutebanken.anshar.routes.siri.handlers.OutboundIdMappingPolicy;
 import no.rutebanken.anshar.routes.siri.transformer.ValueAdapter;
 import no.rutebanken.anshar.subscription.SiriDataType;
@@ -34,33 +35,80 @@ import java.util.concurrent.TimeUnit;
 
 public class OutboundSubscriptionSetup implements Serializable {
 
+    @Setter
+    @Getter
     private SiriValidator.Version siriVersion = SiriValidator.Version.VERSION_2_0;
-    private ZonedDateTime requestTimestamp;
+
+    @Getter
+    private final ZonedDateTime requestTimestamp;
+
+    @Getter
     private final SiriDataType subscriptionType;
+
+    @Getter
     private final String address;
-    private long heartbeatInterval;
-    private int timeToLive;
-    private Map<Class, Set<String>> filterMap;
+
+    @Getter
+    private final long heartbeatInterval;
+
+    @Getter
+    private final Map<Class, Set<String>> filterMap;
+
+    @Getter
     private final List<ValueAdapter> valueAdapters;
+
+    @Getter
     private final String subscriptionId;
-    private String requestorRef;
-    private ZonedDateTime initialTerminationTime;
-    private List<String> datasetList;
-    private String clientTrackingName;
-    private long changeBeforeUpdates;
-    private boolean incrementalUpdates;
-    private long updateInterval;
+
+    @Getter
+    private final String requestorRef;
+
+    @Getter
+    private final ZonedDateTime initialTerminationTime;
+
+    @Getter
+    private final List<String> datasetList;
+
+    @Getter
+    private final String clientTrackingName;
+
+    @Getter
+    private final long changeBeforeUpdates;
+
+    @Getter
+    private final boolean incrementalUpdates;
+
+    @Getter
+    private final long updateInterval;
+
+    @Setter
+    @Getter
     private boolean useOriginalId;
+
+    @Getter
+    @Setter
     private Map<String, List<ValueAdapter>> valueAdaptersByDataset = new HashMap<>();
+
+    @Getter
+    @Setter
     private Map<String, Map<Class, Set<String>>> filterMapByDataset = new HashMap<>();
-    private Cache<String, String> alreadySentNotifications;
+
+    private final Cache<String, String> alreadySentNotifications;
+
+    @Setter
+    @Getter
     private OutboundIdMappingPolicy outboundIdMappingPolicy;
+
+    @Setter
+    @Getter
     private Duration previewInterval;
+
     @Getter
     @Setter
     private CompressionFormat compressionFormat = CompressionFormat.NONE;
 
-
+    @Setter
+    @Getter
     private boolean isSOAPSubscription;
 
     @Getter
@@ -129,145 +177,16 @@ public class OutboundSubscriptionSetup implements Serializable {
         this.isSicAQuaySubscription = isSicAQuaySubscription;
     }
 
-    OutboundSubscriptionSetup(SiriDataType subscriptionType, String address, int timeToLive, List<ValueAdapter> outboundAdapters, String subscriptionId) {
-        this.subscriptionType = subscriptionType;
-        this.address = address;
-        this.timeToLive = timeToLive;
-        this.valueAdapters = outboundAdapters;
-        this.subscriptionId = subscriptionId;
-    }
-
-    public String createRouteId() {
-        return "outbound." + subscriptionType + "." + subscriptionId;
-    }
-
-    public ZonedDateTime getRequestTimestamp() {
-        return requestTimestamp;
-    }
-
-    public SiriDataType getSubscriptionType() {
-        return subscriptionType;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public long getHeartbeatInterval() {
-        return heartbeatInterval;
-    }
-
-    int getTimeToLive() {
-        return timeToLive;
-    }
-
-    public long getChangeBeforeUpdates() {
-        return changeBeforeUpdates;
-    }
-
-    public boolean getIncrementalUpdates() {
-        return incrementalUpdates;
-    }
-
-    public long getUpdateInterval() {
-        return updateInterval;
-    }
-
-    public Map<Class, Set<String>> getFilterMap() {
-        return filterMap;
-    }
-
-    public String getSubscriptionId() {
-        return subscriptionId;
-    }
-
-    public String getRequestorRef() {
-        return requestorRef;
-    }
-
-    public ZonedDateTime getInitialTerminationTime() {
-        return initialTerminationTime;
-    }
-
-    public List<String> getDatasetList() {
-        return datasetList;
-    }
-
-    public List<ValueAdapter> getValueAdapters() {
-        return valueAdapters;
-    }
-
-    public String getClientTrackingName() {
-        return clientTrackingName;
-    }
-
-    public boolean isSOAPSubscription() {
-        return isSOAPSubscription;
-    }
-
-    public void setSOAPSubscription(boolean SOAPSubscription) {
-        isSOAPSubscription = SOAPSubscription;
-    }
-
-    public boolean isUseOriginalId() {
-        return useOriginalId;
-    }
-
-    public void setUseOriginalId(boolean useOriginalId) {
-        this.useOriginalId = useOriginalId;
-    }
-
-
-    public void setValueAdaptersByDataset(Map<String, List<ValueAdapter>> valueAdaptersByDataset) {
-        this.valueAdaptersByDataset = valueAdaptersByDataset;
-    }
-
     public List<ValueAdapter> getValueAdaptersForDatasetd(String datasetId) {
         return valueAdaptersByDataset.containsKey(datasetId) ? valueAdaptersByDataset.get(datasetId) : new ArrayList<>();
-    }
-
-    public void setFilterMapByDataset(Map<String, Map<Class, Set<String>>> filterMapByDataset) {
-        this.filterMapByDataset = filterMapByDataset;
-    }
-
-    public Map<String, Map<Class, Set<String>>> getFilterMapByDataset() {
-        return filterMapByDataset;
-    }
-
-    public Map<String, List<ValueAdapter>> getValueAdaptersByDataset() {
-        return valueAdaptersByDataset;
     }
 
     public String toString() {
         return MessageFormat.format("[subscriptionId={0}, clientTrackingName={1}, requestorRef={2}, address={3}]", subscriptionId, clientTrackingName, requestorRef, address);
     }
 
-    public SiriValidator.Version getSiriVersion() {
-        return siriVersion;
-    }
-
-    public void setSiriVersion(SiriValidator.Version siriVersion) {
-        this.siriVersion = siriVersion;
-    }
-
-    public OutboundIdMappingPolicy getOutboundIdMappingPolicy() {
-        return outboundIdMappingPolicy;
-    }
-
-    public void setOutboundIdMappingPolicy(OutboundIdMappingPolicy outboundIdMappingPolicy) {
-        this.outboundIdMappingPolicy = outboundIdMappingPolicy;
-    }
-
     public boolean hasNotificationBeenAlreadySent(String notificationId) {
         return alreadySentNotifications.getIfPresent(notificationId) != null;
-    }
-
-    public Duration getPreviewInterval() {
-        return previewInterval;
-    }
-
-    public void setPreviewInterval(Duration previewInterval) {
-        this.previewInterval = previewInterval;
     }
 
     public void recordNotification(String notificationId) {
