9b998162097515ef3d54e337d817454be3c239e8
afreche <alexandre.freche@lumiplan.com>
2026-09-14 16:22:30 +0200

    chore(): remove unused method

--- src/main/java/no/rutebanken/anshar/util/DateUtils.java
+++ src/main/java/no/rutebanken/anshar/util/DateUtils.java
@@ -1,12 +1,9 @@
 package no.rutebanken.anshar.util;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
-import java.util.Optional;
 
 @Slf4j
 public class DateUtils {
@@ -16,24 +13,5 @@ public class DateUtils {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
         return ZonedDateTime.parse(dateTimeString, formatter);
     }
-
-    public static Optional<ZonedDateTime> convertGtfsTimeToZonedDateTime(String gtfsTime) {
-        if (StringUtils.isBlank(gtfsTime)) {
-            return Optional.empty();
-        }
-        String[] splitTime = gtfsTime.split(":");
-        if (splitTime.length != 3) {
-            log.error("Invalid GTFS time format: {}", gtfsTime);
-            return Optional.empty();
-        }
-        try {
-            int secondsFromStartOfDay = 3600 * Integer.parseInt(splitTime[0]) + 60 * Integer.parseInt(splitTime[1]) + Integer.parseInt(splitTime[2]);
-            ZonedDateTime startOfDay = ZonedDateTime.now().truncatedTo(ChronoUnit.DAYS);
-            return Optional.of(startOfDay.plusSeconds(secondsFromStartOfDay));
-        } catch (NumberFormatException e) {
-            log.error("Invalid GTFS time format: {}", gtfsTime);
-            return Optional.empty();
-        }
-    }
-
+    
 }
