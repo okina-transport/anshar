eb80db5539da2cf0e44c6b95bb9a8d6724e3dccd
afreche <alexandre.freche@lumiplan.com>
2026-09-22 17:41:06 +0200

    fix(DATA-2202): disable redelivering a message to KAFKA broker when it fails (eg: when broker is down)

--- src/main/java/no/rutebanken/anshar/routes/kafka/KafkaRouteBuilder.java
+++ src/main/java/no/rutebanken/anshar/routes/kafka/KafkaRouteBuilder.java
@@ -1,6 +1,5 @@
 package no.rutebanken.anshar.routes.kafka;
 
-import no.rutebanken.anshar.config.AnsharConfiguration;
 import no.rutebanken.anshar.data.util.CustomSiriXml;
 import no.rutebanken.anshar.routes.dataformat.SiriDataFormatHelper;
 import org.apache.camel.LoggingLevel;
@@ -11,8 +10,6 @@ import uk.org.siri.siri21.Siri;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Set;
-
-import static no.rutebanken.anshar.routes.validation.validators.Constants.DATASET_ID_HEADER_NAME;
 
 @Component
 public class KafkaRouteBuilder extends RouteBuilder {
@@ -26,21 +23,21 @@ public class KafkaRouteBuilder extends RouteBuilder {
     public static final String SEND_TH_TR_CONSISTENCY_REPORT_TO_KAFKA = "direct:send.th.tr.consistency.report.to.kafka";
     public static final String SEND_TR_IN_SUBSCRIPTION_DATA_TO_KAFKA = "direct:send.tr.in.subscription.data.to.kafka";
     public static final String SEND_TR_IN_SUBSCRIPTION_MONITORING_TO_KAFKA = "direct:send.tr.in.subscription.monitoring.to.kafka";
+    private static final String KAFKA_DISABLED_STUB = "stub:nowhere";
 
     private final KafkaConfig kafkaConfig;
-    private final AnsharConfiguration config;
     private final OperatorService operatorService;
     private final KeepOnlyKafkaHeaders keepOnlyKafkaHeaders;
 
-    public KafkaRouteBuilder(KafkaConfig kafkaConfig, AnsharConfiguration config, OperatorService operatorService, KeepOnlyKafkaHeaders keepOnlyKafkaHeaders) {
+    public KafkaRouteBuilder(KafkaConfig kafkaConfig, OperatorService operatorService, KeepOnlyKafkaHeaders keepOnlyKafkaHeaders) {
         this.kafkaConfig = kafkaConfig;
-        this.config = config;
         this.operatorService = operatorService;
         this.keepOnlyKafkaHeaders = keepOnlyKafkaHeaders;
     }
 
     @Override
     public void configure() throws Exception {
+        errorHandler(noErrorHandler());
         if (kafkaConfig.isKafkaEnabled() && kafkaConfig.isSendSiriSmInToKafka()) {
             from(SEND_SM_IN_TO_KAFKA)
                     .marshal(SiriDataFormatHelper.getThreadSafeSiriJaxbDataformat())
@@ -48,7 +45,7 @@ public class KafkaRouteBuilder extends RouteBuilder {
                     .wireTap(kafkaConfig.createCamelProducerConfig(kafkaConfig.getSmInTopic()));
         } else {
             from(SEND_SM_IN_TO_KAFKA)
-                    .to("stub:nowhere") // does nothing but required otherwise camel crash @ start-up
+                    .to(KAFKA_DISABLED_STUB) // does nothing but required otherwise camel crash @ start-up
                     .end();
         }
 
@@ -59,7 +56,7 @@ public class KafkaRouteBuilder extends RouteBuilder {
                     .wireTap(kafkaConfig.createCamelProducerConfig(kafkaConfig.getSxInTopic()));
         } else {
             from(SEND_SX_IN_TO_KAFKA)
-                    .to("stub:nowhere") // does nothing but required otherwise camel crash @ start-up
+                    .to(KAFKA_DISABLED_STUB) // does nothing but required otherwise camel crash @ start-up
                     .end();
         }
 
@@ -74,7 +71,7 @@ public class KafkaRouteBuilder extends RouteBuilder {
                     .wireTap(kafkaConfig.createCamelProducerConfig(kafkaConfig.getGmInTopic()));
         } else {
             from(SEND_GM_IN_TO_KAFKA)
-                    .to("stub:nowhere") // does nothing but required otherwise camel crash @ start-up
+                    .to(KAFKA_DISABLED_STUB) // does nothing but required otherwise camel crash @ start-up
                     .end();
         }
 
@@ -101,7 +98,7 @@ public class KafkaRouteBuilder extends RouteBuilder {
                     .wireTap(kafkaConfig.createCamelProducerConfig(kafkaConfig.getSmOutTopic()));
         } else {
             from(SEND_SM_OUT_TO_KAFKA)
-                    .to("stub:nowhere") // does nothing but required otherwise camel crash @ start-up
+                    .to(KAFKA_DISABLED_STUB) // does nothing but required otherwise camel crash @ start-up
                     .end();
         }
 
@@ -116,7 +113,7 @@ public class KafkaRouteBuilder extends RouteBuilder {
                     .wireTap(kafkaConfig.createCamelProducerConfig(kafkaConfig.getGmOutTopic()));
         } else {
             from(SEND_GM_OUT_TO_KAFKA)
-                    .to("stub:nowhere") // does nothing but required otherwise camel crash @ start-up
+                    .to(KAFKA_DISABLED_STUB) // does nothing but required otherwise camel crash @ start-up
                     .end();
         }
 
